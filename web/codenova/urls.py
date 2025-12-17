40e146570a5983e5ebd81694487ce7a30978846f
ohlee1118@naver.com <ohlee1118@naver.com>
2025-12-17 14:20:36 +0900

    Fix: Media_URL로 변경

--- web/codenova/urls.py
+++ web/codenova/urls.py
@@ -23,5 +23,4 @@ urlpatterns = [
 
 # 개발 환경에서 정적/미디어 서빙
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
